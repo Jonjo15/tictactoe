696ac95665520cada43e438800d4f8331f7db338
IVAN <ivanbatur5g@gmail.com>
2020-05-21 20:15:49 +0200

    Added some background logic to javascript file

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ let Gameboard = (() => {
         return Array.apply(null, Array(9));
     }
 
-    let Player = (name) => {
-        let playedMoves = []
+    let playerFactory = (name, sign) => {
+        playedMoves = []
+        //this.sign = sign;
+        const getSign = () => sign
         const getName = () => name;
-        const addMove = (position) => playedMoves.push(position);
-        return { getName, addMove}
+        const addMove = (position) => this.playedMoves.push(position);
+        return { getName, addMove, getSign}
     }
+
+    playersArray = [];
+    let currentPlayer;
 
     let flowControl = (()=> {
         //switchturns function
@@ -25,14 +30,44 @@ let Gameboard = (() => {
         return {divs, firstPlayerInput, secondPlayerInput, submitButton};
     })();
 
+    function changeCurrentPlayer() {
+        if (currentPlayer == playersArray[0]) {
+            currentPlayer = playersArray[1];
+        }
+        else {
+            currentPlayer = playersArray[0];
+        }
+    }
+
     let bindEvents = (() => {
         cacheDom.submitButton.addEventListener("click", (e) => {
-            console.log("works");
+            if (playersArray.length > 1) { 
+                return;
+            }
+            //console.log(cacheDom.secondPlayerInput.value);
+            let firstPlayerName = cacheDom.firstPlayerInput.value;
+            let firstPlayer = playerFactory(firstPlayerName, "X");
+            playersArray.push(firstPlayer);
+            //console.log(cacheDom.firstPlayerInput.value);
+            let secondPlayerName = cacheDom.secondPlayerInput.value;
+            let secondPlayer = playerFactory(secondPlayerName, "O");
+            playersArray.push(secondPlayer);
+            currentPlayer = playersArray[0];
+        })
+        cacheDom.divs.forEach((div) => {
+            div.addEventListener("click", (e) => {
+                console.log(e.target.dataset.id)
+                //if current player = player1 textContent = x else textcontet = O
+                //changeCurrentPlayer();
+                //check if gameover
+            })
         })
     })();
+    
     /* return {
         board
     } */
+    return {playersArray}
 })();
 //let divs = document.querySelectorAll(".divs")
 //console.log(divs);
